fa4d66ae6baf7a40166b02f151b18bc41e4bf16c
Dennison Bertram <dennison@dennisonbertram.com>
2019-02-06 10:36:31 -0300

    added secrets file, updated truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,47 @@
-'use strict';
+
+"use strict";
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var secrets = require("./secrets.js");
+var apiRinkeby = secrets.apiRinkeby;
+var apiMainnet = secrets.apiMainnet;
+var mnemonic = secrets.mnemonic;
+
+console.log("Mnemonic is: ", mnemonic);
 
 module.exports = {
   networks: {
     local: {
-      host: 'localhost',
+      host: "localhost",
       port: 9545,
       gas: 5000000,
       gasPrice: 5e9,
-      network_id: '*'
+      network_id: "*"
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          apiMainnet
+        );
+      },
+      gas: 200000000,
+      network_id: 1
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          apiRinkeby
+        );
+      },
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: 4
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.11"  // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ }
 };
